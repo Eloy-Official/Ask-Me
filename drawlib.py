b5b7e94ef6a75a56db5e8b894282daeb41946e78
Eloy <110917584+Eloy-Official@users.noreply.github.com>
2023-01-18 20:07:42 +0100

    main

--- drawlib.py
+++ drawlib.py
@@ -2,7 +2,63 @@ import turtle
 import time
 import random
 
-def cube():
-    turtle.forward(100)
+turtle.Screen().bgcolor("black")
+turtle.color("green")
+
+def cube(size):
+    turtle.forward(size)
     turtle.right(90)
-    turtle.forward(100)
+    turtle.forward(size)
+    turtle.right(90)
+    turtle.forward(size)
+    turtle.right(90)
+    turtle.forward(size)
+    turtle.right(45)
+    turtle.forward(size / 2)
+    turtle.right(45)
+    turtle.forward(size)
+    turtle.right(135)
+    turtle.forward(size / 2)
+    turtle.backward(size / 2)
+    turtle.left(45)
+    turtle.forward(size)
+    turtle.right(45)
+    turtle.forward(size / 2)
+    turtle.backward(size / 2)
+    turtle.right(45)
+    
+    turtle.exitonclick()
+    
+    
+#cube()
+def glascube(size):
+    turtle.forward(size)
+    turtle.right(90)
+    turtle.forward(size)
+    turtle.right(90)
+    turtle.forward(size)
+    turtle.right(90)
+    turtle.forward(size)
+    turtle.right(45)
+    turtle.forward(size / 2)
+    turtle.right(45)
+    turtle.forward(size)
+    turtle.right(135)
+    turtle.forward(size / 2)
+    turtle.backward(size / 2)
+    turtle.left(45)
+    turtle.forward(size)
+    turtle.right(45)
+    turtle.forward(size / 2)
+    turtle.backward(size / 2)
+    turtle.right(45)
+    turtle.forward(size)
+    turtle.right(90)
+    turtle.forward(size)
+    turtle.backward(size)
+    turtle.left(135)
+    turtle.forward(size / 2)
+    
+    turtle.exitonclick()
+    
+glascube(int(input("Size: ")))
